ba2c6f8d7394e9773f512d978819e7822d72bc18
redford-jones <redford-jones@outlook.com>
2016-04-19 22:39:38 +0100

    Update config.js

--- bin/config.js
+++ bin/config.js
@@ -19,10 +19,10 @@ config.logger.level = '[:date] :remote-addr :method :url HTTP/:http-version :sta
 config.logger.rotationFrequency = 'daily';
 config.logger.dateFormat = 'YYYYMMDD';
 config.logger.verbose = false;
-config.logger.sentrydsn = 'https://c6654bdfd4e94ba184b33552ee2ed21d:d0078df30b314e5b8aa3cfd78b88aec3@app.getsentry.com/74179';
+config.logger.sentrydsn = xxxx;
 
 //Google Maps
-config.gmaps.key = 'AIzaSyANkUG9Q7YKNV_eTBTM0iBPjLOMbSU6JsY';
+config.gmaps.key = xxxx;
 config.gmaps.version = '3';
 
 module.exports = config;
